524f62b545d50f0bd2642fb03a3480989fde8804
JaimeTadeo <deugartebutron@gmail.com>
2025-04-28 22:58:21 -0400

    feat: primeros tests disponibilidad de gasolina

---
+++ src/gasolinera.js
@@ -0,0 +1,7 @@
+export default function gasolinera(click) {
+    if (click) {
+        return "1000 litros disponibles";
+    } else {
+        return "Ver disponibilidad de gasolina";
+    }
+}
--- src/presenter.js
+++ src/presenter.js
@@ -1,16 +1,15 @@
-import numeros from "./NumerosRomanos";
 //import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+//const first = document.querySelector("#primer-numero");
+//const second = document.querySelector("#segundo-numero");
+//const form = document.querySelector("#sumar-form");
+//const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+//form.addEventListener("submit", (event) => {
+ // event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+//  const firstNumber = Number.parseInt(first.value);
+ // const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+//  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+//});
