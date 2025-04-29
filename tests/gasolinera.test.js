34c46d7b8339b74f157b6fc447d809df75730d66
JaimeTadeo <deugartebutron@gmail.com>
2025-04-28 23:23:37 -0400

    fix:tests pasados

--- tests/gasolinera.test.js
+++ tests/gasolinera.test.js
@@ -1,4 +1,4 @@
-import gasolinera from "../src/gasolinera";
+import gasolinera from "../src/gasolinera"; // o "./gasolinera" si está en la misma carpeta
 
 describe("Gasolinera", () => {
     it("debería mostrar la disponibilidad de gasolina", () => {
@@ -7,5 +7,10 @@ describe("Gasolinera", () => {
 
     it("debería mostrar litros disponibles al hacer click", () => {
         expect(gasolinera(true, 500)).toEqual("500 litros disponibles");
+    });
+
+    it("debería mostrar litros diferentes si cambia la cantidad", () => {
+        expect(gasolinera(true, 300)).toEqual("300 litros disponibles");
+        expect(gasolinera(true, 800)).toEqual("800 litros disponibles");
     });
 });
