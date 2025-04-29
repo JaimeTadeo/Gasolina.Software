93ff1f7b75580f9a5f5bdc83a85592387f8d6115
JaimeTadeo <deugartebutron@gmail.com>
2025-04-28 23:35:20 -0400

    feat:muestra cantidad de gasolina y bolivianos en el html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const boton = document.getElementById("mostrarDisponibilidad");
 const resultado = document.getElementById("resultado");
 
 let click = false;
-let litrosDisponibles = 1000; // Puedes cambiarlo si quieres
+let litrosDisponibles = 1000; // Puedes cambiarlo para pruebas
 
 boton.addEventListener("click", () => {
     click = true;
