f576c5fc0adfdce6acc3fb8b931151718b19336f
Cristian Mendoza <cristian.mendoza.h1@gmail.com>
2025-05-05 23:12:53 -0400

    feat: se aniadio la segunda prueba

--- tests/ticket.test.js
+++ tests/ticket.test.js
@@ -8,15 +8,4 @@ describe('Generación de Tickets', () => {
   it('debe generar un ticket con el número 1 al presionar el botón por primera vez', () => {
     expect(generarTicket()).toBe(1);
   });
-
-  it('debe generar un ticket con el número 2 al presionar el botón por segunda vez', () => {
-    generarTicket();
-    expect(generarTicket()).toBe(2);
-  });
-
-  it('debe generar un ticket con el número 3 al presionar el botón por tercera vez', () => {
-    generarTicket();
-    generarTicket();
-    expect(generarTicket()).toBe(3);
-  });
 });
