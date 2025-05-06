40e7f7f6450402524469a1d43eb3c35f5dd6a4ba
Cristian Mendoza <cristian.mendoza.h1@gmail.com>
2025-05-05 22:21:48 -0400

    feat: nueva funcionalidad llamda reportar surtidor

---
+++ tests/reportarSurtidor.test.js
@@ -0,0 +1,7 @@
+import { reportarSurtidorSinGasolina, obtenerReportesDeSurtidores } from './reportarSurtidor';
+
+describe('Reportar Surtidor Sin Gasolina', () => {
+  beforeEach(() => {
+    global.reportesSurtidores = [];
+  });
+});
