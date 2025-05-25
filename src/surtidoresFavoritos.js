2fea512a39f400ffac08c44ddd3e82c54362685e
Lorena-Carpio-Es <karylore1@gmail.com>
2025-05-25 19:03:58 -0400

    feat: implementa sistema de surtidores favoritos pt2

--- src/surtidoresFavoritos.js
+++ src/surtidoresFavoritos.js
@@ -1,3 +1,12 @@
+const favoritosPorUsuario = {};
+
 export function gestionarSurtidoresFavoritos(usuarioId, surtidorId) {
-    return 'Surtidor 1 agregado a favoritos';
+    if (!favoritosPorUsuario[usuarioId]) {
+        favoritosPorUsuario[usuarioId] = [];
+    }
+    if (!favoritosPorUsuario[usuarioId].includes(surtidorId)) {
+        favoritosPorUsuario[usuarioId].push(surtidorId);
+        return `Surtidor ${surtidorId} agregado a favoritos`;
+    }
+    return `El surtidor ${surtidorId} ya está en tus favoritos`;
 }
